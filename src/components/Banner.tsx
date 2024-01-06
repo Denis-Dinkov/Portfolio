aa1df17146fef2e9bc75db52a849c17ef1498e3e
Denis Dinkov <denisdinkov7@gmail.com>
2024-01-06 13:48:15 +0200

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
 
 export default function Banner() {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleted] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState();
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  const tick = () => {
+    
+  }
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker)
+    }
+  });
   return (
     <section className="banner" id="home">
       <Container>
@@ -11,7 +34,12 @@ export default function Banner() {
               {`H1 im web developer`} <span className="wrap">test</span>
             </h1>
             <p>QWEQWEQWEQE!@#@!#@#@!@#!#!</p>
-            <button onClick={() => console.log("asdaad")}>Let's connect </button>
+            <button onClick={() => console.log("asdaad")}>
+              Let's connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
           </Col>
         </Row>
       </Container>
