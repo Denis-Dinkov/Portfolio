19e898ce1c8c0015659a84555088f12f24e9c3cb
Denis Dinkov <denisdinkov7@gmail.com>
2024-01-13 12:25:02 +0200

    Project section

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,21 +1,41 @@
+import { CodeIcon, EyeIcon } from "@heroicons/react/solid";
+import { Link } from "react-router-dom";
 export default function ProjectCard({
-  imgUrl,
+  img,
   title,
   description,
+  tags,
 }: {
-  imgUrl: string;
+  img: string;
   title: string;
   description: string;
+  tags: string[];
 }) {
   return (
     <div>
-      <img 
-        className="h-52 md:h-72 rounded-t-xl"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      ></img>
-      <div className="text-white rounded-b-xl"></div>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group" // Added w-full for width
+        style={{ background: `url(${img})`, backgroundSize: "cover" }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+          <Link
+            to="/"
+            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB8BE] hover:border-white group/link"
+          >
+            <CodeIcon className="h-10 w-10 text-[#ADB8BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+          </Link>
+          <Link
+            to="/"
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB8BE] hover:border-white group/link"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB8BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+          </Link>
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text=[#ADB7BE]">{description}</p>
+      </div>
     </div>
   );
 }
--- src/Components/ProjectsSection.tsx
+++ src/Components/ProjectsSection.tsx
@@ -1,12 +1,16 @@
 import ProjectCard from "./ProjectCard";
 
+import Project1 from "../assets/images/projects/1.png";
+import Project2 from "../assets/images/projects/2.png";
+import Project3 from "../assets/images/projects/3.png";
+
 const projectsData = [
   {
     id: 1,
     title: "Project 1",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, laborum.",
-    image: "../assets/images/projects/1.png",
+    image: Project1,
     tag: ["All", "React"],
   },
   {
@@ -14,7 +18,7 @@ const projectsData = [
     title: "Project 2",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, laborum.",
-    image: "../assets/images/projects/2.png",
+    image: Project2,
     tag: ["All", "React"],
   },
   {
@@ -22,7 +26,7 @@ const projectsData = [
     title: "Project 3",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, laborum.",
-    image: "../assets/images/projects/3.png",
+    image: Project3,
     tag: ["All", "React"],
   },
 ];
@@ -30,16 +34,26 @@ const projectsData = [
 export default function ProjectSection() {
   return (
     <>
-      <h2>My Projects</h2>
-      <div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">
+          All
+        </button>
+        <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">
+          Web
+        </button>
+        <button>Web</button>
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
         {projectsData.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
-            imgUrl={project.image}
+            img={project.image}
             tags={project.tag}
-
           />
         ))}
       </div>
